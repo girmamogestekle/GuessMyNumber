879574d4ac2b0e7466070c3947bef5429321037a
GirmaMogesTekle <girma.moges.teklemariam@outlook.com>
2024-07-16 20:06:10 -0500

    Update script file

---
+++ script.js
@@ -0,0 +1,42 @@
+'use strict';
+console.log(document.querySelector('.message').textContent);
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+function displayMessageAndScore(message) {
+  document.querySelector('.message').textContent = message;
+  let score = Number(document.querySelector('.score').textContent);
+  score--;
+  if (score >= 1) {
+    document.querySelector('.score').textContent = score;
+  } else {
+    document.querySelector('.message').textContent = '🔥 You lost the game!';
+    document.querySelector('.score').textContent = 0;
+  }
+}
+
+function displayCorrectNumber() {
+  document.querySelector('.message').textContent = '🎉 Correct Number!';
+  document.querySelector('body').style.backgroundColor = '#60b347';
+  document.querySelector('.number').style.width = '30rem';
+  document.querySelector('.number').textContent = secretNumber;
+}
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) document.querySelector('.message').textContent = '⛔️ No Number!';
+  else if (guess === secretNumber) displayCorrectNumber();
+  else if (guess > secretNumber) displayMessageAndScore('📈 Too High!');
+  else if (guess < secretNumber) displayMessageAndScore('📉 Too Low!');
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.guess').value = '';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+});
